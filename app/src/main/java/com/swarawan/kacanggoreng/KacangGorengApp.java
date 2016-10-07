399fa79a9faab1f2901658a4058dc9da858a9abc
Rio Swarawan <rioswarawan@alienator.local.tonjoo.com>
2016-10-07 16:49:14 +0700

    add sample app and update gitignore file

---
+++ app/src/main/java/com/swarawan/kacanggoreng/KacangGorengApp.java
@@ -0,0 +1,13 @@
+package com.swarawan.kacanggoreng;
+
+import android.app.Application;
+
+/**
+ * Created by rioswarawan on 10/6/16.
+ */
+public class KacangGorengApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/swarawan/kacanggoreng/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.swarawan.kacanggoreng.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.swarawan.kacanggoreng.R;
+
+/**
+ * Created by rioswarawan on 10/6/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
